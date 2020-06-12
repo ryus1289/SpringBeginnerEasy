a6d32731ca16ee4f9b4c10762f6c3b751071435f
ryus1 <ryus1@DESKTOP-0MAMVTJ>
2020-06-12 23:10:34 +0900

    セクション５ 演習完了

--- src/main/java/com/example/demo/app/survey/SurveyForm.java
+++ src/main/java/com/example/demo/app/survey/SurveyForm.java
@@ -2,56 +2,56 @@ package com.example.demo.app.survey;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class SurveyForm{
 
-	/*Add parameters(0~150) 引数を追加(0~150)*/
+    @Min(0) @Max(150)
     private int age;
-    
+
     /*Add parameters(1~5) 引数を追加(1~5)*/
+    @Min(1) @Max(5)
     private int satisfaction;
 
-    /*Add parameters(200 characters or less) 引数を追加(200文字以内)*/
+    @Size(min = 1, max = 200, message="Please input 200 characters or less")
     private String comment;
-	
-	public SurveyForm() {};
-	
+
+    public SurveyForm() {};
+
     public SurveyForm(int age, int satisfaction, String comment) {
-		this.age = age;
-		this.satisfaction = satisfaction;
-		this.comment = comment;
-	}
+        this.age = age;
+        this.satisfaction = satisfaction;
+        this.comment = comment;
+    }
 
 
-	public int getAge() {
-		return age;
-	}
+    public int getAge() {
+        return age;
+    }
 
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    public void setAge(int age) {
+        this.age = age;
+    }
 
 
-	public int getSatisfaction() {
-		return satisfaction;
-	}
+    public int getSatisfaction() {
+        return satisfaction;
+    }
 
 
-	public void setSatisfaction(int satisfaction) {
-		this.satisfaction = satisfaction;
-	}
+    public void setSatisfaction(int satisfaction) {
+        this.satisfaction = satisfaction;
+    }
 
 
-	public String getComment() {
-		return comment;
-	}
+    public String getComment() {
+        return comment;
+    }
 
 
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 
 }
