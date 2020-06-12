60903825a033d8402507fe856d9cd79d11d8d433
poirot <ryus1289@gmail.com>
2020-06-12 18:37:11 +0900

    新規登録

--- src/main/java/com/example/demo/app/SampleController.java
+++ src/main/java/com/example/demo/app/SampleController.java
@@ -1,5 +1,7 @@
 package com.example.demo.app;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -7,24 +9,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/*
- * Add annotations here
- */
+@Controller
+@RequestMapping("/sample")
 public class SampleController {
-	
-// 	private final JdbcTemplate jdbcTemplate;
 
-// 	//Add an annotation here 
-// 	public SampleController(JdbcTemplate jdbcTemplate) {
-// 		this.jdbcTemplate = jdbcTemplate;
-// 	}
-	
-	@GetMapping
-	public String test(Model model) {
-		
-		//hands-on
+    private final JdbcTemplate jdbcTemplate;
 
-		return "test";
-	}
+    @Autowired
+    public SampleController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping("/test")
+    public String test(Model model) {
+
+        String sql = "select id, name, email "
+                + "from inquiry where id = 1";
+
+        Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+        model.addAttribute("title", "Hello World");
+        model.addAttribute("name", map.get("name"));
+        model.addAttribute("email", map.get("email"));
+        return "test";
+    }
 
 }
