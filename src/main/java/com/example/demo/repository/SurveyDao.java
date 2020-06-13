84bce1e9b0807a7c9b122f86438daf9a7354a3cb
poirot <ryus1289@gmail.com>
2020-06-14 08:44:45 +0900

    セクション8 完了
    ・警告の解消

--- src/main/java/com/example/demo/repository/SurveyDao.java
+++ src/main/java/com/example/demo/repository/SurveyDao.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.Inquiry;
 import com.example.demo.entity.Survey;
 
 @Repository
 public interface SurveyDao {
-	
-	void insertSurvey(Survey survey);
-	
-	List<Survey> getAll();
-	
+
+    void insertSurvey(Survey survey);
+
+    List<Survey> getAll();
+
 }
--- src/main/java/com/example/demo/service/SurveyService.java
+++ src/main/java/com/example/demo/service/SurveyService.java
@@ -2,13 +2,12 @@ package com.example.demo.service;
 
 import java.util.List;
 
-import com.example.demo.app.survey.SurveyForm;
 import com.example.demo.entity.Survey;
 
 public interface SurveyService {
-	
-	void save(Survey survey);
-	
-	List<Survey> getAll();
+
+    void save(Survey survey);
+
+    List<Survey> getAll();
 
 }
